d55279183771aac7ae0f08875f6ba948a1760c27
Gargantua7 <1505011271@qq.com>
2023-02-08 03:14:56 +0800

    2023/2/7 + 2023/2/8

---
+++ src/leetcode/editor/cn/[1233]删除子文件夹.kt
@@ -0,0 +1,92 @@
+//你是一位系统管理员，手里有一份文件夹列表 folder，你的任务是要删除该列表中的所有 子文件夹，并以 任意顺序 返回剩下的文件夹。 
+//
+// 如果文件夹 folder[i] 位于另一个文件夹 folder[j] 下，那么 folder[i] 就是 folder[j] 的 子文件夹 。 
+//
+// 文件夹的「路径」是由一个或多个按以下格式串联形成的字符串：'/' 后跟一个或者多个小写英文字母。 
+//
+// 
+// 例如，"/leetcode" 和 "/leetcode/problems" 都是有效的路径，而空字符串和 "/" 不是。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+//输出：["/a","/c/d","/c/f"]
+//解释："/a/b/" 是 "/a" 的子文件夹，而 "/c/d/e" 是 "/c/d" 的子文件夹。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：folder = ["/a","/a/b/c","/a/b/d"]
+//输出：["/a"]
+//解释：文件夹 "/a/b/c" 和 "/a/b/d/" 都会被删除，因为它们都是 "/a" 的子文件夹。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+//输出: ["/a/b/c","/a/b/ca","/a/b/d"] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= folder.length <= 4 * 10⁴ 
+// 2 <= folder[i].length <= 100 
+// folder[i] 只包含小写字母和 '/' 
+// folder[i] 总是以字符 '/' 起始 
+// 每个文件夹名都是 唯一 的 
+// 
+//
+// Related Topics 字典树 数组 字符串 👍 82 👎 0
+
+
+package leetcode.editor.cn
+
+import java.util.LinkedList
+
+/**
+ * 1233
+ * 删除子文件夹
+ *
+ * @author Gargantua丶
+ * 2023-02-08 02:57:08
+ */
+class RemoveSubFoldersFromTheFilesystem {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun removeSubfolders(folder: Array<String>): List<String> {
+        val root = Trie()
+        folder.forEachIndexed { i, path ->
+            var curr = root
+            path.split('/').forEach { dir ->
+                curr = curr.children.getOrPut(dir, ::Trie)
+            }
+            curr.index = i
+        }
+
+        val ans = ArrayList<String>()
+        val queue = LinkedList<Trie>()
+        queue.offer(root)
+        while (queue.isNotEmpty()) {
+            val node = queue.poll()
+            if (node.index >= 0) ans.add(folder[node.index])
+            else node.children.forEach { (_, trie) -> queue.offer(trie) }
+        }
+        return ans
+    }
+
+    class Trie {
+        var index: Int = -1
+        val children = HashMap<String, Trie>()
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
