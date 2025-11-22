a72cac63eaab27835a03cea445bb2fc98faded0b
Gargantua7 <gargantua7@qq.com>
2025-11-22 23:13:08 +0800

    2025/11/22

---
+++ src/leetcode/editor/cn/[3190]使所有元素都可以被 3 整除的最少操作数.kt
@@ -0,0 +1,62 @@
+//给你一个整数数组 nums 。一次操作中，你可以将 nums 中的 任意 一个元素增加或者减少 1 。 
+//
+// 请你返回将 nums 中所有元素都可以被 3 整除的 最少 操作次数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：nums = [1,2,3,4] 
+// 
+//
+// 输出：3 
+//
+// 解释： 
+//
+// 通过以下 3 个操作，数组中的所有元素都可以被 3 整除： 
+//
+// 
+// 将 1 减少 1 。 
+// 将 2 增加 1 。 
+// 将 4 减少 1 。 
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：nums = [3,6,9] 
+// 
+//
+// 输出：0 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 50 
+// 1 <= nums[i] <= 50 
+// 
+//
+// Related Topics 数组 数学 👍 15 👎 0
+
+
+package leetcode.editor.cn
+
+/*** 3190
+* 使所有元素都可以被 3 整除的最少操作数
+*
+* @author Gargantua7
+* 2025-11-22 23:10:04
+*/
+class FindMinimumOperationsToMakeAllElementsDivisibleByThree {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun minimumOperations(nums: IntArray): Int {
+        return nums.count { it % 3 != 0 }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
